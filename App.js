87664ee7be9d8e2c40c1c1f1eb9dfc033c2d9678
Vinitasa <95077435+Vinitasa@users.noreply.github.com>
2022-05-17 18:43:30 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header/Header';
+import Login from '../src/Components/Login/Login';
+
+function App() {
+  return (
+    <>
+    <div className='h-100'>
+      {/* <Header /> */}
+      <Login />
+    </div>
+    </>
+  );
+}
+
+export default App;
